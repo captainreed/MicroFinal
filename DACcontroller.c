48d2205620f0450f8275a733fe1583ddd429b7ba
caydjj <45081477+caydjj@users.noreply.github.com>
2018-11-15 15:18:58 -0700

    Update DACcontroller.c

--- DACcontroller.c
+++ DACcontroller.c
@@ -1,8 +1,13 @@
 #include "DACcontroller.h"
 
 
-void initDAC()//initialize the ADC
-{
-	
-	
+void DAC_initialize(void) {
+	RCC->APB1ENR1 |= RCC_APB1ENR1_DAC1EN;
+	DAC->CR &= ~(DAC_CR_EN1 | DAC_CR_EN2);
+	DAC->MCR &= ~(7U<<16);
+	DAC->CR |= DAC_CR_TEN2;
+	DAC->CR |= DAC_CR_TSEL2;
+	DAC->CR |= DAC_CR_EN2;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+	GPIOA->MODER |= 3U<<(2*5); 
 }
