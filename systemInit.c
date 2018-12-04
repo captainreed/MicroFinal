0799ba66727a46dc1a93ccf7307f5360b82acbbd
caydjj <45081477+caydjj@users.noreply.github.com>
2018-12-04 09:46:50 -0700

    Removes PA1 for PA3

--- systemInit.c
+++ systemInit.c
@@ -9,18 +9,16 @@
 void configureInterrupts()
 {
 RCC->AHB2ENR |=RCC_AHB2ENR_GPIOAEN;     //enables GPIO_A clock
-	GPIOA->MODER &=(0xFFFFFFC0);    //sets GPIO A pins 0, 1, and 2 for input mode
-	GPIOA->PUPDR &=(0xFFFFFFC0);    //sets GPIO A pins 0, 1, and 2 to pull-down mode
-  GPIOA->PUPDR |=(0x0000002A);
-	GPIOA->MODER &=(0xFFFFF3FF);    //sets GPIO A pin 5 to output mode
-	GPIOA->MODER |=(0x00000400);
+	GPIOA->MODER &=(0xFFFFFF0C);    //sets GPIO A pins 0, 2, and 3 for input mode
+	GPIOA->PUPDR &=(0xFFFFFF0C);    //sets GPIO A pins 0, 2, and 3 to pull-down mode
+  GPIOA->PUPDR |=(0x00000051);
 	NVIC_EnableIRQ(EXTI0_IRQn);     //enables EXTI0 interrupt
-	NVIC_EnableIRQ(EXTI1_IRQn);     //enables EXTI1 interrupt
+	NVIC_EnableIRQ(EXTI3_IRQn);     //enables EXTI3 interrupt
 	NVIC_EnableIRQ(EXTI2_IRQn);     //enables EXTI2 interrupt
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;   //enables SYSCFG
-	SYSCFG->EXTICR[0] &= (0xFFFFF000);      //ties interrupts to GPIO A
-	EXTI->RTSR1 |= (0x00000007);    //enables rising trigger selection for interrupt 0, 1, and 2
-	EXTI->IMR1 |= (0x00000007);     //enables interrupt mask register for interrupt 0, 1, and 2
+	SYSCFG->EXTICR[0] &= (0xFFFF00F0);      //ties interrupts to GPIO A
+	EXTI->RTSR1 |= (0x0000000B);    //enables rising trigger selection for interrupt 0, 3, and 2
+	EXTI->IMR1 |= (0x0000000B);     //enables interrupt mask register for interrupt 0, 3, and 2
 		
 }
 
