f81655b449c42c8abb0c6defac133982511ba48b
caydjj <45081477+caydjj@users.noreply.github.com>
2018-12-06 13:59:15 -0700

    Update LEDcontroller.c

--- LEDcontroller.c
+++ LEDcontroller.c
@@ -7,12 +7,33 @@
 #include <stdbool.h>
 
 
-void initLED()//initialize the ADC
+void initLED()//initialize the LED pins
 {
-	
-	
+	RCC->AHB2ENR |=RCC_AHB2ENR_GPIOEEN; 
+	GPIOE->MODER &= (0x00FFFFFF);	//sets GPIO pins 12, 13, 14, 15 to output mode
+	GPIOE->MODER |= (0x55000000);
+	GPIOE->PUPDR &= (0x00FFFFFF);	//sets pull-down resistors for 12, 13, 14, 15
+	GPIOE->PUPDR |= (0xAA000000);	
 }
-void writeLED()
+void writeLED(uint16_t data)
 {
-	
+	GPIOE->ODR &= (0xFFFFEFFF);	//disables PE12
+	GPIOE->ODR &= (0xFFFFDFFF);	//disables PE13
+	GPIOE->ODR &= (0xFFFFBFFF);	//disables PE14
+	GPIOE->ODR &= (0xFFFF7FFF);	//disables PE15
+	if(data<= (0x3FF)) {	//lower
+		GPIOE->ODR |= (0x1000);	//enables PE12 
+	} else if(data<= (0x7EE)) {
+		GPIOE->ODR |= (0x1000);	//enables PE12 
+		GPIOE->ODR |= (0x2000);	//enables PE13 
+	} else if(data<= (0xBFD)) {
+		GPIOE->ODR |= (0x1000);	//enables PE12 
+		GPIOE->ODR |= (0x2000);	//enables PE13 
+		GPIOE->ODR |= (0x4000);	//enables PE14
+	} else {
+		GPIOE->ODR |= (0x1000);	//enables PE12 
+		GPIOE->ODR |= (0x2000);	//enables PE13 
+		GPIOE->ODR |= (0x4000);	//enables PE14
+		GPIOE->ODR |= (0x8000);	//enables PE15
+	}	
 }
